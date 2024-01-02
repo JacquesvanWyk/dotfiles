baeecae06ebd569f2a6a5227bde4b4673dee1872
Jacques <jvw679@gmail.com>
2024-01-02 19:40:14 +0200

    updates

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/lua/plugins/neo-tree.lua
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  },
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,16 +6,23 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Atelierforest (dark) (terminal.sexy)"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("Jetbrains Mono")
 config.font_size = 24
+config.line_height = 1.25
 
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.98
 
--- and finally, return the configuration to wezterm
+config.window_padding = {
+	left = 14,
+	right = 14,
+	top = 14,
+	bottom = 0,
+}
+
 return config
